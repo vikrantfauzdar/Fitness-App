176b483640dc3ddb854c0265bd876d2f9ea415da
vikrantfauzdar <vikrant.fauzdar_cs22@gla.ac.in>
2025-07-08 20:48:57 +0530

    App.js

--- fitnessapp/src/App.js
+++ fitnessapp/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider, styled } from "styled-components";
+import {lightTheme} from "./utils/theme";
+import {BrowserRouter, Route, Routes} from "React-router-dom";
+import Authentication from "./page/Authentication";
+import {useState} from "react";
+import {useSelector} from "react-redux";
+import Navbar from "./components/navbar";
+import Dashboard from "./pages/Dashboard";
+import Workkouts from "./pages/Workkouts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+flex-directons: column;
+background: ${({ theme }) => theme.bg};
+color: ${({ theme }) => theme.text_primary};
+overflow-x: hidden;
+overflow-y: hidden;
+transition: all 0.2s ease;
+`;
+
+function App(){
+  const{ currentUser } = useSelector((stste) => StaticRange.user);
+  return(
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+      {currentUser ? (
+        <Container>
+          <Navbar curreentUser={currentUser}/>
+          <Routes>
+            <Route path="/"exact elemment={<Dashboard />}/>
+            <Route path="/workouts" exact element={Workout />} />
+          </Routes>
+        </Container>
+      ):(
+        <Container>
+          <Auntentication/>
+        </Container>
+      )}
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
 export default App;
